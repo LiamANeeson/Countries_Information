4af197804333bf69dc048063efede34d764fab91
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-24 20:37:52 +0100

    Updated Styling on Home Page and Form

--- client/src/Components/CountryForm/CountryForm.js
+++ client/src/Components/CountryForm/CountryForm.js
@@ -1,18 +1,30 @@
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import './CountryForm.css';
 
 function CountryForm(props) {
   return (
-    <Form onSubmit={props.onSubmit}>
-        <input 
-            type = "text"
-            name = "country"
-            placeholder = "Enter a Country"
-            value={props.value}
-            onChange={props.onChange}
-        />
-        <Button type='submit'>Submit</Button>
-    </Form>
+    <Container className="country-form-container">
+      <Form onSubmit={props.onSubmit}>
+        <Row className="justify-content-center">
+          <Col md={6}>
+            <div className="input-container">
+              <input 
+                type="text"
+                name="country"
+                placeholder="Enter a Country"
+                value={props.value}
+                onChange={props.onChange}
+                className="country-input"
+              />
+              <Button variant="primary" type="submit" className="country-submit">
+                Submit
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
-export default CountryForm
+export default CountryForm;
