a3fa06412076c8114bf86072c57d986eb95ca515
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-24 15:30:17 +0100

    Added basic frontend with a form to get conuntry informatino from inputted value

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home"
+import Country from "./Pages/Country"
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/countries/:name" element={<Country />} />
+      </Routes>
+    </Router>
   );
 }
 
