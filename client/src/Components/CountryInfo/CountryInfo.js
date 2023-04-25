3fb4dc01fcf024ae03abb137ffbd24dea55e7b1f
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-25 17:48:44 +0100

    Added additional list items from the backend to frontend

--- client/src/Components/CountryInfo/CountryInfo.js
+++ client/src/Components/CountryInfo/CountryInfo.js
@@ -10,9 +10,17 @@ function CountryInfo(props) {
         <h2>{countryInfo.name.common}</h2>
         <span alt={`${countryInfo.name.common} Flag`}>{countryInfo.flag}</span>
         <ul>
+            <li>Official Name: {countryInfo.name.official}</li>
             <li>Population: {countryInfo.population}</li>
             <li>Capital: {countryInfo.capital[0]}</li>
             <li>Region: {countryInfo.region}</li>
+            <li>Currency: {countryInfo.currencies.EUR.name}</li>
+            <li>Currency Symbol: {countryInfo.currencies.EUR.symbol}</li>
+            <li>Languages: {countryInfo.languages.eng}, {countryInfo.languages.gle}</li>
+            <li>Car Sign: {countryInfo.car.signs}</li>
+            <li>Side of the Road they drive on: {countryInfo.car.side}</li>
+            <li>Timezone: {countryInfo.timezones}</li>
+            <li>Google Maps: {countryInfo.maps.googleMaps}</li>
         </ul>
     </div>
   );
