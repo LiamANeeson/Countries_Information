918ff43d85e3c8a01ee02145676b88f0f758a26e
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-25 18:23:06 +0100

    Added image and dynamically add lanaguages

--- client/src/Components/CountryInfo/CountryInfo.js
+++ client/src/Components/CountryInfo/CountryInfo.js
@@ -7,16 +7,19 @@ function CountryInfo(props) {
 
   return (
     <div>
+        <img src={countryInfo.flags.svg}/>
         <h2>{countryInfo.name.common}</h2>
         <span alt={`${countryInfo.name.common} Flag`}>{countryInfo.flag}</span>
         <ul>
             <li>Official Name: {countryInfo.name.official}</li>
+            <li>Native Name: {countryInfo.name.nativeName.gle.common}</li>
             <li>Population: {countryInfo.population}</li>
             <li>Capital: {countryInfo.capital[0]}</li>
             <li>Region: {countryInfo.region}</li>
             <li>Currency: {countryInfo.currencies.EUR.name}</li>
             <li>Currency Symbol: {countryInfo.currencies.EUR.symbol}</li>
-            <li>Languages: {countryInfo.languages.eng}, {countryInfo.languages.gle}</li>
+            <li>Languages: {Object.values(countryInfo.languages).join(', ')}</li>
+            <li>Is UN Member: {countryInfo.unMember ? 'Yes': 'No'}</li>
             <li>Car Sign: {countryInfo.car.signs}</li>
             <li>Side of the Road they drive on: {countryInfo.car.side}</li>
             <li>Timezone: {countryInfo.timezones}</li>
