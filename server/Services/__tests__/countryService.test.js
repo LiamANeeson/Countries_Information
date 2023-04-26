23060ec1df194f0d3096f587cb0cd25c6eea8e49
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-26 19:13:42 +0100

    Added jest.config.js to run tests, fixed error in conutryService.test.js

--- server/Services/__tests__/countryService.test.js
+++ server/Services/__tests__/countryService.test.js
@@ -5,12 +5,13 @@ jest.mock('axios');
 
 describe('countryService', () => {
   it('Should return country information', async () => {
-    const data = { name: 'Ireland', capital: 'Dublin', population: 4994724 };
-    axios.get.mockResolvedValue({data: data});
+    axios.get.mockImplementation(() =>
+      Promise.resolve({ data: [{ name: 'Ireland', capital: 'Dublin', population: 4994724 }] })
+    );
 
     const result = await countryService.getCountryInfo('Ireland');
 
-    expect(result).toEqual(data);
+    expect(result).toEqual({ name: 'Ireland', capital: 'Dublin', population: 4994724 });
     expect(axios.get).toHaveBeenCalledWith('https://restcountries.com/v3.1/name/Ireland');
   });
 
