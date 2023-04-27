46074b5d21a93da2b40da34e4b9b232343efaec4
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-27 16:42:30 +0100

    Console log

--- client/src/Services/countryService.js
+++ client/src/Services/countryService.js
@@ -6,6 +6,7 @@ export const getCountryInfo = async (name) => {
   const url = `${BASE_URL}/countries/${name}`;
   try {
     const response = await axios.get(url);
+    console.log(response.data)
     return response.data;
   } catch (error) {
     console.log(error);
