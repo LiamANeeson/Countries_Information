83007d083751e044ba7ef2c5d202dda5d7ca26b7
Liam Neeson <55084620+LiamANeeson@users.noreply.github.com>
2023-04-27 12:03:59 +0100

    Version 1.0.0

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const path = require('path')
 const countryRoutes = require("./Routes/countryRoutes")
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 8000;
@@ -10,6 +11,18 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/countries", countryRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
   
 // Start Server
 app.listen(port, () => {
